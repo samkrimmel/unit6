e798ceede8ab2b7dde58a9e6b68e7372c0de9dcd
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-05-11 08:24:23 -0400

    Updated from Brython Server: 5/11/2018 8:24:19 AM

--- warmup16.py
+++ warmup16.py
@@ -4,7 +4,7 @@
 
 file = open('engmix.txt')
 
-for line.strip() in file:
-    word = line.split(' ')
-    if line[0] == 's' and line[-1] == 'k':
-        print(line)
+for line in file:
+    word = line.split()
+    if word[0] == 's' and word[-1] == 'k':
+        print(word)
