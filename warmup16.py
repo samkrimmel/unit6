afa21cdb9a4f86f4c9a6dc4b35250db59b520974
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-05-11 08:20:49 -0400

    Updated from Brython Server: 5/11/2018 8:20:45 AM

--- warmup16.py
+++ warmup16.py
@@ -1,1 +1,9 @@
+#Sam Krimmel
+#5/11/18
+#warmup16.py - prints out all words that start with initial and end with last initial
 
+file = open('engmix.txt')
+
+for line in file:
+    if line[0] == 's' and line[-1] == 'k':
+        print(line)
