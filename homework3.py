4f6c7334f9bd29f5b4df1f52a19cbaf7883de413
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-05-14 08:33:35 -0400

    Updated from Brython Server: 5/14/2018 8:33:33 AM

--- homework3.py
+++ homework3.py
@@ -1,1 +1,10 @@
+#Sam Krimmel
+#5/14/18
+#homework3.py - add ! to the end of each line in input.txt
 
+file = open('homework1.py')
+
+for line in file:
+    line = line.strip() + '!'
+    
+print(file)
