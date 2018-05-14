4bdb5d0711158050d632f251e30ef3748882e3ed
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-05-14 08:07:10 -0400

    Updated from Brython Server: 5/14/2018 8:07:08 AM

--- homework1.py
+++ homework1.py
@@ -1,1 +1,11 @@
+#Sam Krimmel
+#5/14/18
+#homework1.py - ask user for word and tell them whether or not it is in dictionary file
 
+file = open('engmix.txt')
+
+word = input('Enter a word: ')
+
+for line in file:
+    if word == line:
+        print('Your word is in the dictionary!')
