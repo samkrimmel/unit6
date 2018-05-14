b51c9577f9d2f3714ca532d57742e00a554f1433
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-05-14 08:18:17 -0400

    Updated from Brython Server: 5/14/2018 8:18:15 AM

--- homework2.py
+++ homework2.py
@@ -1,1 +1,14 @@
+#Sam Krimmel
+#5/14/18
+#homework2.py - print number word entered from the dictionary
+
+file = open('engmix.txt')
+
+fileList = []
+for line in file:
+    fileList.append(line.strip())
+
+numWord = int(input('Enter a number of a word in the dictionary: '))
+
+print(fileList[numWord-1])
 
