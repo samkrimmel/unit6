83c8d6d0283b542e477556197864e91a8cd9326a
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-05-14 08:41:44 -0400

    Updated from Brython Server: 5/14/2018 8:41:43 AM

--- homework4.py
+++ homework4.py
@@ -1,1 +1,14 @@
+#Sam Krimmel
+#5/14/18
+#homework4.py
+
+file = open('engmix.txt')
+
+letter = input('Enter a letter: ')
+
+bestNum = ''
+for line in file:
+    line.strip()
+    if bestNum.count(letter) < line.count(letter):
+        bestNum = line
 
