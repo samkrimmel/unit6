ab884a223ef61b2e4c0e0795884b6a807037a3c3
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-05-16 08:32:45 -0400

    Updated from Brython Server: 5/16/2018 8:32:42 AM

--- palindromes.py
+++ palindromes.py
@@ -1,1 +1,15 @@
+#Sam Krimmel
+#5/16/18
+#palindromes.py - prints out all palindromes
 
+file = open('engmix.txt')
+
+palindromes = []
+for line in file:
+    L = []
+    L.append(line.strip())
+    if L == L.reverse():
+        palindromes.append(line.strip())
+
+for item in palindromes:
+    print(item)
