65187f4e7e340bc6362ed5f2ad17eb1cdb02610f
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-05-17 08:43:19 -0400

    Updated from Brython Server: 5/17/2018 8:43:15 AM

--- longShort.py
+++ longShort.py
@@ -1,1 +1,14 @@
+#Sam Krimmel
+#5/17/18
+#longShort.py - prints longest and shortest word for each letter of the alphabet
 
+file = open('engmix.txt')
+
+for ch in 'abcdefghijklmnopqrstuvwxyz':
+    L = []
+    for line in file:
+        line = line.strip()
+        while ch == line[0]:
+            L.append(line)
+    print(max(L))
+    print(min(L))
