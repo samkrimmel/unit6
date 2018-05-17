acac274f5177875c6761e146dcb57471eb10cdcb
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-05-17 08:15:23 -0400

    Updated from Brython Server: 5/17/2018 8:15:20 AM

--- warmup17.py
+++ warmup17.py
@@ -1,1 +1,13 @@
+#Sam Krimmel
+#5/17/18
+#warmup17.py - dununu dununu nu nu nuuu do do doooooo
 
+file = open('engmix.txt.)
+
+letrs = 0
+for line in file:
+    for letter in 'krimelKRIMEL':
+        if letter in line:
+            letrs += 1
+    if letrs >= 6:
+        print line
