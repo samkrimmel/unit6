3d7086bd466ad345c50e9413a930da88c3ac9d89
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-05-17 08:27:42 -0400

    Updated from Brython Server: 5/17/2018 8:27:38 AM

--- palindromes.py
+++ palindromes.py
@@ -5,9 +5,10 @@
 file = open('engmix.txt')
 
 palindromes = []
+
 for line in file:
     back = ''
-    for ch in line:
+    for ch in line.strip():
         back = ch + back
     if line == back:
         palindromes.append(line.strip())
