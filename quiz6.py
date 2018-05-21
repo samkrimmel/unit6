4978cd7c833e39adf1ac84276bf9a72e7813ed25
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-05-21 08:06:38 -0400

    Updated from Brython Server: 5/21/2018 8:06:37 AM

--- quiz6.py
+++ quiz6.py
@@ -4,10 +4,19 @@
 
 file = open('engmix.txt')
 
+"""
+#Program 2:
 for line in file:
     line = line.strip()
     if len(line) >= 9:
         if line[0] == line[4] and line[4] == line[8]:
             print(line)
             break
+"""
+letter = input('Enter a letter: ')
+num = int(input('Enter a number: '))
 
+for line in file:
+    line = line.strip()
+    if len(line) == num and line[0] == letter:
+        print(line)
