f23c4b3d38a7ad7b2e21eca8a7f43099e6679836
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-05-21 08:03:54 -0400

    Updated from Brython Server: 5/21/2018 8:03:53 AM

--- quiz6.py
+++ quiz6.py
@@ -1,1 +1,12 @@
+#Sam Krimmel
+#5/21/18
+#quiz6.py
+
+file = open('engmix.txt')
+
+for line in file:
+    line = line.strip()
+    if len(line) >= 9:
+        if line[0] == line[4] and line[4] == line[8]:
+            print(line)
 
